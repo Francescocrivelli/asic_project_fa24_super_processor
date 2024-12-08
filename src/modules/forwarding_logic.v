0545297e6ea37ea2e7e41eb4e421dc3883af0d46
Matias Barraza <mbarraza2020@berkeley.edu>
2024-12-07 21:00:36 -0800

    saved now

--- src/modules/forwarding_logic.v
+++ src/modules/forwarding_logic.v
@@ -6,7 +6,24 @@ module ForwardingLogic(
 
 );
 
+// Opcode values
+reg [6:0] X_opcode;
+reg [6:0] D_opcode;
+reg [6:0] Mem_WB_opcode;
+
+// rd values
+reg [4:0] X_rd;
+reg [4:0] D_rd;
+reg [4:0] Mem_WB_rd;
+
+
+
 always @(*) begin
+    // Check if previous instruction needs current register data
+    // if (X stage register == Mem_wb rd)
+        // (if rs1) forward path from output of ALU to input of A mux 
+        // (if rs2) forward ALU out to B mux
+    
     
 end
 
