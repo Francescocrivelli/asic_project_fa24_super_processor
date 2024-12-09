16c6a090d1b6e3ad1f52a02f397912e34d363952
Francesco Crivelli <francesco.crivelli21@gmail.com>
2024-12-09 02:40:34 -0800

    Bit masking

--- src/modules/mem_wb_stage/MemWBLogic.v
+++ src/modules/mem_wb_stage/MemWBLogic.v
@@ -5,7 +5,14 @@ module MemWBLogic (
     output reg RegWEn
 );
 
+`define FUNCT3_BYTE   3'b000
+`define FUNCT3_HALF   3'b001
+`define FUNCT3_WORD   3'b010
+
 always@(*) begin
+    write_mask = 4'b0000;
+    WBSel = 2'b00;
+    RegWEn = 0;
     case (opcode)
         `OPC_ARI_RTYPE, `OPC_ARI_ITYPE, `OPC_AUIPC: begin
             WBSel = 2'b01;
@@ -14,6 +21,25 @@ always@(*) begin
         `OPC_STORE: begin
             WBSel = 2'b00;
             RegWEn = 1;
+
+            case (funct3)
+                `FUNCT3_BYTE: begin
+                    // SINGLE Byte
+                    write_mask = (4'b0001 << addr_lsb);
+                end
+                `FUNCT3_HALF: begin
+                    //HALF WORD
+                    case (addr_lsb[1])
+                        1'b0: write_mask = 4'b0011; // Lower half-word
+                        1'b1: write_mask = 4'b1100; // Upper half-word
+                    endcase
+                end
+                `FUNCT3_WORD: begin
+                    //Write the word
+                    write_mask = 4'b1111;
+                end
+                default: write_mask = 4'b0000; 
+            endcase
         end
         `OPC_BRANCH: begin
             WBSel = 2'b00;
